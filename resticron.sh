cf766e6b68b50fdb77af1fafff126dc71077790a
Jim Dersch <dncrjim@gmail.com>
2024-04-02 16:40:28 -0400

    update config file creator

--- resticron.sh
+++ resticron.sh
@@ -2,6 +2,7 @@
 
 ### CHECK TO SEE IF resticron_config.sh EXISTS, GENERATE, EMAIL, AND EXIT IF NOT
 
+#only works if you navigate to the correct folder before running
 if [ ! -f "resticron_config.sh" ]; then
   echo "#!/bin/bash" > resticron_config.sh
   echo "" >> resticron_config.sh
@@ -18,6 +19,9 @@ if [ ! -f "resticron_config.sh" ]; then
   echo "restic_backup_command_2=\"\"" >> resticron_config.sh
   echo "restic_prune_command=\"\"" >> resticron_config.sh
   echo "restic_check_command=\"\"" >> resticron_config.sh
+  echo "" >> resticron_config.sh
+  echo "#restic does not automatically report progress to log, set value for reporting frequency, 1 / length of time in seconds, so 1 is every second, 0.016666 is once per minute." >> resticron_config.sh
+  echo "restic_progress_reporting=0.016666   # default: 0.016666" >> resticron_config.sh
   echo "resticron_config.sh was not found. A template file was created, please insert the values for all variables and run again." | tee >(mail -s "resticron Setup Necessary" root)
   exit 0
 fi
@@ -26,20 +30,18 @@ fi
 . ./resticron_config.sh
 export RESTIC_PROGRESS_FPS="$restic_progress_reporting"
 
-
 ### CHECK TO SEE IF ANOTHER INSTANCE IS RUNNING, CLOSE AND SEND EMAIL IF THERE IS
 
-# Count the number of instances of this script running excluding the current instance (current_pid) from the count
+# Count the number of instances of this script running excluding the current instance
 # Note: this line will only work if you are running this script as sudo and don't change the process name
 num_instances=$(pgrep -cxf "sudo /bin/bash resticron.sh")
 
-# If there's more than one instance running (including the current one), exit
+# If there's more than one instance running, send error email and exit
 if [ "$num_instances" -gt 1 ]; then
       # following line both prints to terminal and sends email
     echo "An instance of resticron was already running when this script was started. If this is unexpected, check that an existing instance has not hung while processing." | tee >(mail -s "resticron ERROR" $email)
     exit 1
 fi
-
 
 # send stdout+stderr to log file
 exec > >(tee -a "$log_file") 2>&1
